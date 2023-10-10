7c307dda40d712704b81946c65254dc3e763c8b1
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-10 15:00:52 -0300

    Add files via upload

---
+++ cars/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    factory_year = models.IntegerField()
+    model_year = models.IntegerField()
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.model
---
+++ cars/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarModelForm
+
+
+def cars_view(request):
+    cars = Car.objects.all().order_by('brand')
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+
+    return render(request,
+                  'cars.html',
+                  {'cars': cars})
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
