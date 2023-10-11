4938c19436768f9b4b94231348a660cfc597d46b
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 11:54:39 -0300

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -4,18 +4,6 @@ from cars.forms import CarModelForm
 from django.views import View
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('brand')
-    search = request.GET.get('search')
-
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-
-    return render(request,
-                  'cars.html',
-                  {'cars': cars})
-
-
 class CarView(View):
 
     def get(self, request):
