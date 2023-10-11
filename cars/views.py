9a715370c4b1649611f79014253d3c730a7c0894
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 13:55:38 -0300

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -18,12 +18,15 @@ class CarView(View):
                       {'cars': cars})
 
 
-def new_car_view(request):
-    if request.method == 'POST':
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
