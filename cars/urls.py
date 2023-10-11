781816867488b945bc7b700c78ae5ea5f38c143a
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 14:16:13 -0300

    Update urls.py

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarView, NewCarView
+from cars.views import CarView, NewCarView, CarsListView
 
 
 urlpatterns = [
     path('cars/', CarView.as_view(), name='cars_list'),
-    path('new_car/', NewCarView.as_view(), name='new_car')
+    path('new_car/', CarsListView.as_view(), name='new_car')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
