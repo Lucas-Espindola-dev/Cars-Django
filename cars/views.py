caaeadfd4b093fc4da494ca5bb643620b18845ff
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 14:37:21 -0300

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -5,20 +5,6 @@ from django.views import View
 from django.views.generic.list import ListView
 
 
-class CarView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('brand')
-        search = request.GET.get('search')
-
-        if search:
-            cars = Car.objects.filter(model__icontains=search).order_by('model')
-
-        return render(request,
-                      'cars.html',
-                      {'cars': cars})
-
-
 class CarsListView(ListView):
     model = Car
     template_name = 'cars.html'
