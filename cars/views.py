5deceb738d03cf14fa31143c4becbf0ad8ddca39
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-17 11:18:13 -0300

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 
 class CarsListView(ListView):
@@ -9,6 +9,8 @@ class CarsListView(ListView):
     context_object_name = 'cars'
 
     def get_queryset(self):
+        # Nesse query, todos os modelos são retornados da mesma forma que retorna no objects.all().
+        # O super faz referência a ListView. A utilização de super() é para invocar a herança.
         cars = super().get_queryset().order_by('model')
         search = self.request.GET.get('search')
         if search:
@@ -21,9 +23,16 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+    # Pagina seguinte.
 
 
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
-    
+
+
+class CarUpdate(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
