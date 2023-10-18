565e03558c6807d78960763517ce40bf0bddc7ea
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-18 09:12:11 -0300

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 
 class CarsListView(ListView):
@@ -9,6 +10,8 @@ class CarsListView(ListView):
     context_object_name = 'cars'
 
     def get_queryset(self):
+        # Nesse query, todos os modelos são retornados da mesma forma que retorna no objects.all().
+        # O super faz referência a ListView. A utilização de super() é para invocar a herança.
         cars = super().get_queryset().order_by('model')
         search = self.request.GET.get('search')
         if search:
@@ -21,6 +24,7 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+    # Pagina seguinte.
 
 
 class CarDetailView(DetailView):
@@ -28,8 +32,16 @@ class CarDetailView(DetailView):
     template_name = 'car_detail.html'
 
 
-class CarUpdate(UpdateView):
+class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
     success_url = '/cars/'
