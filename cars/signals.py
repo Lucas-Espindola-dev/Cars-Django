b52406965920c956ad12383c691d0fb2be25043b
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-19 11:23:33 -0300

    Update signals.py

--- cars/signals.py
+++ cars/signals.py
@@ -1,23 +1,25 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models.signals import post_save, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('### PRE SAVE ###')
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value,
+    )
 
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('### POST SAVE ###')
-
-
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('### PRE DELETE ###')
+    car_inventory_update()
 
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print('### POST DELETE ###')
+    car_inventory_update()
