c85604972ec8529d645b511baea1eb95a0999a63
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-19 10:31:07 -0300

    Update models.py

--- cars/models.py
+++ cars/models.py
@@ -21,3 +21,15 @@ class Car(models.Model):
 
     def __str__(self):
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
